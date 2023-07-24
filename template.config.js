319bcff6749f06b7319d81a2e8d55d8c6b512abc
devucb <91523007+devucb@users.noreply.github.com>
2023-07-24 23:31:23 +0300

    chore: init template

---
+++ template.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    placeholderName: 'Boilerplate',
+    titlePlaceholder: 'React Native Boilerplate',
+    templateDir: './template',
+    postInitScript: './post-init.script.js',
+};
